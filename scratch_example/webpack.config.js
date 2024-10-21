ab159ba9a32dfad4164cfec3f7f75311a544df9d
Sarthak Bakshi <sarthakbakshi@Sarthaks-MacBook-Air.local>
2024-10-21 20:28:19 +0530

    [LEARNING][BASIC_UNDERSTANDING]: added complete flow

--- scratch_example/webpack.config.js
+++ scratch_example/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
 module.exports = {
   entry: "./app/index.js",
   module: {
@@ -14,7 +15,7 @@ module.exports = {
       },
       {
         test: /\.(js)$/,
-        use: "bable-loader",
+        use: "babel-loader",
       },
     ],
   },
@@ -23,4 +24,5 @@ module.exports = {
     filename: "bundle.js",
   },
   plugins: [new HtmlWebpackPlugin()],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
